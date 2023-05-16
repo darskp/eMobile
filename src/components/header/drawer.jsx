b875cb8cfa603282918d5f71b4d12d2cbca95c04
darskp <darshanpatgar100@gmail.com>
2023-05-16 14:14:56 +0530

    router added

--- src/components/header/drawer.jsx
+++ src/components/header/drawer.jsx
@@ -1,23 +1,38 @@
 
 import { Drawer, List, ListItemButton, ListItemIcon,ListItemText ,ListItem,IconButton} from '@mui/material'
 import {useState} from 'react';
+import { Link as RouterLink } from 'react-router-dom';
+
 import MenuIcon from '@mui/icons-material/Menu';
 const DrawerComp = ({pages}) => {
     const [openDrawer, setopenDrawer] = useState(false);
-    console.log(pages);
     return (<>
         <Drawer anchor='top' open={openDrawer} onClose={() => setopenDrawer(false)} sx={{zIndex: '1',transition:"0.5s all" }}>
             <List sx={{ marginTop: "50px",transition:"0.5s all"}}>
                 {
                 pages.map((data,index)=>{
                     return (
-                    <ListItem disablePadding key={index}>
-                        <ListItemButton onClick={()=>{setopenDrawer(false)}}>
-                            <ListItemIcon>
-                                <ListItemText >{data}</ListItemText>
-                            </ListItemIcon>
-                        </ListItemButton>
-                    </ListItem>)
+                        <RouterLink
+                            to={data.link}
+                            style={{
+                                textDecoration: "none",
+                            }}
+                            sx={{
+                                "&:focus, &:hover, &:visited, &:link, &:active": {
+                                    backgroundColor: "none !important",
+                                    // Add any other active styles you want to remove
+                                },
+                            }}
+                        >
+                    <ListItem disablePadding key={data.id}>
+                                <ListItemButton onClick={() => { setopenDrawer(false) }}>
+                                    <ListItemIcon>
+                                        <ListItemText>{data.name}</ListItemText>
+                                    </ListItemIcon>
+                                </ListItemButton>
+                    </ListItem>
+                            </RouterLink>
+                    )
                 })
                 }
             </List>
--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,8 +1,38 @@
-import { AppBar, Toolbar, Tabs, Tab, Button, useTheme, useMediaQuery, Typography } from '@mui/material';
-import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
+import { AppBar, Toolbar, Tabs, Tab, Button, useTheme, useMediaQuery, Typography, Grid } from '@mui/material';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import { useState } from 'react';
 import Drawer from './drawer';
-const PAGES = ['Home', 'Contact Us', "Admin", "Login", "Sign Up"]
+import { Link as RouterLink } from 'react-router-dom';
+const PAGES =
+    [
+        {
+            id: 1,
+            link: '/',
+            name: "Home"
+        },
+        {
+            id: 2,
+            link: '/contact',
+            name: "Contact Us"
+        },
+        {
+            id: 3,
+            link: '/admin',
+            name: "Admin"
+        },
+        {
+            id: 4,
+            link: '/login',
+            name: "Login"
+        },
+        {
+            id: 5,
+            link: '/signup',
+            name: "Sign Up"
+        }
+    ]
+
+
 const Header = () => {
     const [value, setvalue] = useState(null)
     let theme = useTheme()
@@ -12,10 +42,36 @@ const Header = () => {
             {
                 ismatch ?
                     <>
-                        <AppBar sx={{ background: "#063970", padding: "0px 20px" }}>
+                        <AppBar sx={{ background: "#063970", padding: "0px 10px" }}>
                             <Toolbar>
-                                <AddShoppingCartIcon sx={{ fontSize: "30px" }} />
-                                <Drawer pages={PAGES} />
+                                <Grid
+                                    container
+                                    alignItems="center"
+                                >
+                                    <Grid item xs={10}>
+                                        <Typography
+                                            component={RouterLink}
+                                            to="/"
+                                            sx={{
+                                                fontSize: "25px",
+                                                textDecoration: "none",
+                                                color: "inherit",
+                                                "&:hover":{
+                                                textDecoration:"underline",
+                                                transition:"0.5s"
+                                                }
+                                            }}
+                                        >
+                                            eMobile
+                                        </Typography>
+                                    </Grid>
+                                    <Grid item xs={1}>
+                                        <ShoppingCartIcon sx={{ marginTop: "7px" }} />
+                                    </Grid>
+                                    <Grid item xs={1}>
+                                        <Drawer pages={PAGES} />
+                                    </Grid>
+                                </Grid>
                             </Toolbar>
                         </AppBar>
                     </>
@@ -23,26 +79,78 @@ const Header = () => {
                     <>
                         <AppBar sx={{ background: "#063970", padding: "0px 40px" }}>
                             <Toolbar>
-                                <AddShoppingCartIcon sx={{ fontSize: "30px" }} />
-                                <Tabs sx={{ marginLeft: "auto" }} textColor='inherit' value={value} indicatorColor="dark" onChange={(e, val) => { setvalue(val) }}>
+                                <Grid
+                                    container
+                                    direction="row"
+                                    justifyContent="space-between"
+                                    alignItems="center"
+                                >
+                                    <Grid item xs={3}>
+                                        <Typography
+                                            sx={{
+                                                fontSize: "25px",
+                                                textDecoration: "none",
+                                                color: "inherit",
+                                                "&:hover": {
+                                                    textDecoration: "underline",
+                                                    transition: "0.5s"
+                                                }
+                                            }}
+                                            component={RouterLink}
+                                            to="/"
 
-                                    {
-                                        PAGES.filter((data, index) => {
-                                            return (index < 3)
-                                        }).map((data, index) => {
-                                            return (<Tab key={index} label={data} sx={{ padding: "0px 40px" }} />
-                                            )
-                                        })
-                                    }
-                                </Tabs>
-                                <Button sx={{ marginLeft: "auto" }} variant="contained">Login</Button>
-                                <Button sx={{ marginLeft: "10px" }} variant="contained">SignUp</Button>
+                                        >
+                                            eMobile
+                                        </Typography>
+                                    </Grid>
+                                    <Grid item xs={6}>
+                                        <Tabs sx={{ marginLeft: "auto" }} textColor='inherit' value={value} indicatorColor="dark" onChange={(e, val) => { setvalue(val) }}>
+                                            {
+                                                PAGES.filter((data, index) => {
+                                                    return (index < 3)
+                                                }).map((data, index) => {
+                                                    return (
+                                                        <Tab key={data.id} label={data.name} component={RouterLink} to={data.link} sx={{ padding: "0px 40px", textDecoration: "none" }} />)
+                                                })
+                                            }
+                                        </Tabs>
+                                    </Grid>
+                                    <Grid item xs={3}>
+
+                                        <Grid
+                                            container
+                                            direction="row"
+                                            justifyContent="space-between"
+                                            alignItems="center"
+                                        >
+
+                                            <Grid item xs={4}>
+                                                    <Button variant="outlined" size="small" component={RouterLink} to="/contact">Login</Button>
+                                            </Grid>
+                                            <Grid item xs={4}>
+                                                <Button variant="contained" sx={{ letterSpacing: "1px" }} size="small" component={RouterLink} to="/contact">SignUp</Button>
+                                            </Grid>
+                                            <Grid item xs={4}>
+                                                <RouterLink to="/cart" style={{ textDecoration: "none" }}>
+                                                    <ShoppingCartIcon
+                                                        sx={{
+                                                            fontSize: "30px",
+                                                            marginTop: "9px",
+                                                            marginLeft: "25px",
+                                                            color: "white"
+                                                        }}
+                                                    />
+                                                </RouterLink>
+                                                                                          </Grid>
+                                        </Grid>
+                                    </Grid>
+                                </Grid>
                             </Toolbar>
                         </AppBar>
                     </>
-}
-</>
-)
+            }
+        </>
+    )
 }
 
 export default Header;
